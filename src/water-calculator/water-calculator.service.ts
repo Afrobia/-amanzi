2ae241534d1fdf5e952d109926d50af47a6667b9
Beatriz Santana <169806953+Afrobia@users.noreply.github.com>
2024-10-17 00:45:24 +0000

    ajuste de service

--- src/water-calculator/water-calculator.service.ts
+++ src/water-calculator/water-calculator.service.ts
@@ -38,49 +38,47 @@ export class WaterCalculatorService {
     }
   }
 
-  statusTemperature(temperature: number): boolean {
+  statusTemperature(temperature: number): StatusCodes {
     if (temperature >= 18 || temperature <= 29.9) {
-      return false;
+      return StatusCodes.Safe;
     } else if (temperature >= 30) {
-      return true;
+      return StatusCodes.Unsafe;
     }
   }
 
-  statusHumidity(humidity: number): boolean {
+  statusHumidity(humidity: number): StatusCodes {
     if (humidity >= 40 || humidity <= 70) {
-      return false;
+      return StatusCodes.Safe;
     } else if (humidity <= 39.9) {
-      return true;
+      return StatusCodes.Unsafe;
     }
   }
 
-  statusClima(temperature: number, humidity: number): boolean {
+  async isClimateUnsafe(temperature: number, humidity: number): Promise<boolean> {
     const sHumidity = this.statusHumidity(humidity);
     const sTempeture = this.statusTemperature(temperature);
-    if (sHumidity == true && sTempeture == true) {
+    if (sHumidity == StatusCodes.Unsafe || sTempeture == StatusCodes.Unsafe) {
       return true;
-    } else if (sHumidity == true && sTempeture == false) {
-      return true;
-    } else if (sHumidity == false && sTempeture == true) {
-      return true;
-    }
-    return false;
+    } else return false;
   }
 
   async getWaterIntake(weight: number, city: string, state: string): Promise<WaterIntake> {
     const clima = await this.getClima(city, state)
     const intake = await this.calculateWaterIntake(weight)
     let message = ''
-    const climateUnsafe = await this.statusClima(clima.averageTemperature, clima.relativeHumidity)
+    const climateUnsafe = await this.isClimateUnsafe(clima.averageTemperature, clima.relativeHumidity)
     if (climateUnsafe == true) {
-       message = "Alerta de risco a sáude, recomendo ingerir 4,5L litros de agua hoje, dado as condições climaticas de onde mora."
-    }
-       message = `Recomenda-se ingerir ${intake}L no dia, condições climáticas dentro da normalidade`
-   
-    const newAlert = new WaterIntake(weight,city,state,clima.relativeHumidity,clima.averageTemperature,message)
+      message = "Alerta de risco a sáude, recomendo ingerir 4,5L litros de agua hoje, dado as condições climaticas de onde mora."
+    } else
+      message = `Recomenda-se ingerir ${intake}L no dia, condições climáticas dentro da normalidade`
+
+    const newAlert = new WaterIntake(weight, city, state, clima.relativeHumidity, clima.averageTemperature, message)
 
     return newAlert
   }
-
 }
 
+enum StatusCodes {
+  Safe,
+  Unsafe
+};
