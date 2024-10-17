b0fa889cfa8f8d27129354d207d1f79a1dd92d85
AvaCorrea <avapcorrea@outlook.com>
2024-10-17 06:18:59 -0300

    Commiting unfinished test due to problems developing

--- src/geoclimate/test/geoclimate.service.spec.ts
+++ src/geoclimate/test/geoclimate.service.spec.ts
@@ -2,13 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GeoclimateService } from '../application/geoclimate.service';
 import { HttpService } from '@nestjs/axios/dist/http.service';
 import { ConfigService } from '@nestjs/config';
+import AxiosResponse from 'axios';
+import { get } from 'http';
 
 describe('GeoclimateService', () => {
   let service: GeoclimateService;
   let httpService: HttpService;
   let configService: ConfigService;
-  const mockHttpService = {};
-  const mockConfigService = {};
+  const mockHttpService = {
+    subscribe: jest.fn(),
+    get: jest.fn()
+  };
+  const mockConfigService = {
+    getOrThrow: jest.fn()
+  };  
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,4 +33,17 @@ describe('GeoclimateService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  /*it('should get climate data', async () => {
+    const city = 'São Paulo';
+    const state = 'SP';
+    const completeResponse = {data: {main: {temp: 25, humidity: 70}}};
+
+    jest.spyOn(mockHttpService, 'get').mockResolvedValue(completeResponse);
+    const response = await service.getClimate(city, state);
+    expect(response).toEqual({averageTemperature: 25, relativeHumidity: 70});
+
+    expect(mockHttpService).toHaveBeenCalledTimes(1);
+    expect(service.getClimate).toHaveBeenCalledWith(city, state);
+  });*/
 });
