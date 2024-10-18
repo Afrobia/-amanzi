32d1dfab25e85560adef7afdfd3e07c624128a47
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-10-18 13:50:29 -0300

    delete domain auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,14 @@
-import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { AuthService } from './auth/auth.service';
-import { LoginDto } from './auth/loginDto';
 
 @Controller()
 @ApiTags('Home')
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-  ) {}
-  
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  @ApiOperation({summary: "Boas-vindas "})
+  @ApiOperation({ summary: 'Boas-vindas ' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,18 @@ import { UsersModule } from './users/application/users.module';
 import { AppService } from './app.service';
 import { GeoclimateModule } from './geoclimate/application/geoclimate.module';
 import { DbModule } from './db/db.module';
-import { AuthModule } from './auth/auth.module';
 import { WaterCalculatorModule } from './water-calculator/water-calculator.module';
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal:true, envFilePath: '.env' }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     DbModule,
     UsersModule,
     GeoclimateModule,
     WaterCalculatorModule,
-    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule {}
