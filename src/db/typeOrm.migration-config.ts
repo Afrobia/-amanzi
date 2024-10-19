aebc7bd9684cc8ffe48a9ca07243fec1f313e928
AvaCorrea <avapcorrea@outlook.com>
2024-10-19 02:31:03 -0300

    trying to fix connection

--- src/db/typeOrm.migration-config.ts
+++ src/db/typeOrm.migration-config.ts
@@ -7,7 +7,7 @@ config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  port: 6543,
+  port: 5432,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
